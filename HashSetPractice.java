5ef6f86b48394a1d93afcc943aa1d0eb52a22c84
Abhishek Prajapati <60133606+AbhiPrajapati@users.noreply.github.com>
2022-08-22 09:54:35 +0530

    file added

---
+++ HashSetPractice.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+
+public class HashSetPractice {
+    public static void main(String[] args) {
+		HashSet<Integer> hs = new HashSet<>();
+		hs.add(20);
+		hs.add(30);
+		hs.add(50);
+		System.out.println(hs);
+	}
+}
